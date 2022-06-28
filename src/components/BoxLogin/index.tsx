0bb185923cff4dd629af0ca89057745f71e03294
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-06-27 21:43:56 -0300

    Componente BoxLogin

--- src/components/BoxLogin/index.tsx
+++ src/components/BoxLogin/index.tsx
@@ -1,7 +1,25 @@
 import * as S from './style';
 
+
 const BoxLogin = () => {
-	return <S.BoxLogin></S.BoxLogin>;
+	return (
+		<S.BoxLogin>
+			<S.BoxLoginLogo>
+				<S.BoxLoginLogoImage
+					src={require('assets/img/logo.png')}
+					alt="Image do logo com um desenho de um controle de video game"
+				/>
+			</S.BoxLoginLogo>
+			<S.BoxLoginForm>
+				<input type="email" placeholder="Coloque seu email..." />
+				<input type="password" placeholder="Coloque sua senha..." />
+				<p>
+					Não tem uma conta? <a href="#">Crie uma!</a>
+				</p>
+				<S.BoxLoginButton>Entrar</S.BoxLoginButton>
+			</S.BoxLoginForm>
+		</S.BoxLogin>
+	);
 };
 
 export default BoxLogin;
--- src/components/BoxLogin/style.ts
+++ src/components/BoxLogin/style.ts
@@ -2,8 +2,56 @@ import styled, { css } from 'styled-components';
 
 export const BoxLogin = styled.section`
 	${({ theme }) => css`
-		background: ${theme.colors.baseBg1};
+		background: ${theme.colors.baseForm2};
+		width: 508px;
+		height: 427px;
+		padding: 20px 20px 35px 20px;
+		margin: 20px;
+		border-radius: 8px;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
 		box-shadow: 3px 3px 6px 3px ${theme.colors.shadowColor};
-		color: ${theme.colors.textColor};
+		color: ${theme.colors.textInput};
+	`}
+`;
+
+export const BoxLoginLogo = styled.div`
+	${() => css`
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+	`}
+`;
+
+export const BoxLoginLogoImage = styled.img`
+	${() => css`
+		width: 15%;
+	`}
+`;
+
+export const BoxLoginForm = styled.form`
+	${({ theme }) => css`
+		display: flex;
+		flex-direction: column;
+		gap: 15px;
+
+		input {
+			${theme.mixins.input};
+			align-self: center;
+		}
+
+		p {
+			margin-top: -10px;
+			margin-left: 14%;
+		}
+	`}
+`;
+
+export const BoxLoginButton = styled.button`
+	${({ theme }) => css`
+		${theme.mixins.button1}
+
+		align-self: center;
 	`}
 `;
