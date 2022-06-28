7002f5c5d0da785771228e2fa54d0d51a6d4ef5b
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-06-27 21:46:31 -0300

    criando mixins de input e botão1

--- src/assets/styles/mixins.ts
+++ src/assets/styles/mixins.ts
@@ -8,6 +8,32 @@ const logoStyle = () => css`
 	line-height: ${constants.logoLineHeight};
 `;
 
+const input = () => css`
+	width: 362px;
+	height: 43px;
+	background-color: ${colors.baseInput};
+	color: ${colors.textInput};
+	border-radius: 7px;
+	font-size: 18px;
+	padding-left: 10px;
+	border: solid 1px #ddd;
+	margin-bottom: 30px;
+`;
+
+const button1 = () => css`
+	width: 142px;
+	height: 45px;
+	text-transform: uppercase;
+	background: ${colors.buton1};
+	color: ${colors.textColor};
+	font-size: 22px;
+	border-radius: 8px;
+	font-weight: 400;
+	line-height: 26px;
+`;
+
 export const mixins = {
 	logoStyle,
+	input,
+	button1,
 };
