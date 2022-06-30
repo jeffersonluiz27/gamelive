8c6ae97085b5e6430008bf34609d506590044be8
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-06-30 19:13:49 -0300

    pagina new game

--- src/pages/NewGame/index.tsx
+++ src/pages/NewGame/index.tsx
@@ -1,7 +1,13 @@
+import HeaderLogo from 'components/HeaderLogo';
 import * as S from './style';
 
 const NewGame = () => {
-	return <S.NewGame></S.NewGame>;
+	return (
+		<S.NewGame>
+			<HeaderLogo />
+			<S.NewGameContent></S.NewGameContent>
+		</S.NewGame>
+	);
 };
 
 export default NewGame;
--- src/pages/NewGame/style.ts
+++ src/pages/NewGame/style.ts
@@ -9,3 +9,22 @@ export const NewGame = styled.section`
 		height: 100vh;
 	`}
 `;
+
+export const NewGameContent = styled.main`
+	${({ theme }) => css`
+		${theme.mixins.overlay};
+		width: 100%;
+		height: 70%;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+		align-items: center;
+
+		h2 {
+			font-size: 40px;
+			font-weight: 400;
+			line-height: 46px;
+			margin-bottom: 0px;
+		}
+	`}
+`;
