d9e834777212db5eff936711208e1f2cc325ce30
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-06-30 10:56:48 -0300

    update colors

--- src/assets/styles/colors.ts
+++ src/assets/styles/colors.ts
@@ -1,26 +1,19 @@
 export const colors = {
 	primaryColor: '#6e9e66',
-
-	baseBg1: '#10141d',
-
 	primaryColorOpacity: 'rgba(110,158,102,0.4)',
-
 	shadowColor: '#6e9e6666',
-
+	baseBg1: '#10141d',
+	baseBg2: '#c1c1c1',
 	baseForm1: '#9c9791',
 	baseForm2: '#ffffff',
 	baseInput: '#f2f2f2',
 	textInput: '#2d303e',
-
 	textColor: '#ffffff',
 	textLighter: '#e0e6e9',
 	textLight: '#abbbc2',
 	textNeutral: '#889898',
 	textDark: '#3b5162',
-
 	butonPurple: '#836ed7',
 	butonRed: '#f30445',
-
-	color2: '#fdb205',
-	color3: '#c1c1c1',
+	buttonGold: '#fdb205',
 };
