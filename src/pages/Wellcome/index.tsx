17974d71c16a07cf0d15268daabdcc1d7882547c
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-06-30 00:17:57 -0300

    botão para login

--- src/pages/Wellcome/index.tsx
+++ src/pages/Wellcome/index.tsx
@@ -1,9 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import * as S from './style';
 
 const Wellcome = () => {
+	const navigate = useNavigate();
+
+	function goToLogin() {
+		navigate('/login');
+	}
+
 	return (
 		<S.Wellcome>
 			<h1>Pagina Publica de boas vindas e link para login</h1>
+			<button onClick={goToLogin}>Login</button>
 		</S.Wellcome>
 	);
 };
