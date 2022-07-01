ec61653fd19ee43de344b718200296872a4d52e0
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-07-01 17:01:51 -0300

    Implementando botão entrar

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,23 @@
 import * as S from './style';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import BoxLogin from 'components/BoxLogin';
 import Logo from 'components/Logo';
+import { RoutePath } from 'types/routes';
 
 const Login = () => {
+	const [errorMessage, setErrorMessage] = useState('');
+	const navigate = useNavigate();
+
+	const handleSubmit = () => {
+		navigate(RoutePath.HOME);
+	};
+
 	return (
 		<S.Login>
 			<Logo />
 			<S.LoginContent>
-				<BoxLogin />
+				<BoxLogin onSubmitData={handleSubmit} errorMessage={errorMessage} />
 			</S.LoginContent>
 		</S.Login>
 	);
