ef58f09ab1b4e9367b6ff01824de948591b72816
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-07-09 09:18:01 -0300

    Auth login e cadastro obs arrumar swal cadastro

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,17 +1,33 @@
 import api from './api';
+
 interface userLoginObj {
 	email: string;
-	senha: string;
+	password: string;
+}
+
+interface userObj {
+	name: string;
+	cpf: string;
+	email: string;
+	password: string;
+	confirmPassword: string;
+	isAdmin: boolean;
 }
 
 const loginService = {
 	login: (values: userLoginObj) =>
 		api
 			.post('/auth', values)
-			.then((response: any) => {
-				return response;
-			})
+			.then((response: any) => response)
 			.catch((error: any) => console.log('ERRO NA CHAMADA:', error)),
 };
 
-export { loginService };
+const registerService = {
+	registerValues: (values: userObj) =>
+		api
+			.post('/user', values)
+			.then((response: any) => response)
+			.catch((error: any) => error.response),
+};
+
+export { loginService, registerService };
